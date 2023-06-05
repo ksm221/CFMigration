accda6e381e25f87c069010561950c2f62811979
Karl <karl.mellikov@gmail.com>
2023-06-06 00:55:41 +0300

    all

---
+++ CFMigration/Controllers/RpgCharacterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CFMigration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RpgCharacterController : ControllerBase
+    {
+    }
+}
---
+++ CFMigration/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CFMigration.Data
+{
+    public class DataContext : DbContext
+    {
+        //konstruktor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        //tulemuste nägemiseks dbset
+        public DbSet<RpgCharacter> RpgCharacters => Set<RpgCharacter>();
+    }
+}
